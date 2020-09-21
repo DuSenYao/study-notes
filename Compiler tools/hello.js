fe702a66d57876bcd21508a3cbeb1f254f9f0cd7
1758231591 <1758231591@qq.com>
2020-09-21 20:52:32 +0800

    update VSCode

---
+++ Compiler tools/hello.js
@@ -0,0 +1,11 @@
+msg = "Hello World";
+console.log(msg);
+/**
+ * Document JSDoc描述
+ * 在其他地方引用相应函数时，可以显示详细的函数提示
+ * @param {number} a Some number
+ * @param {string} b string
+ */
+function hello(a, b) {
+  return;
+}
