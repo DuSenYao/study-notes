359f6817570537fd47cce985738e855921682107
1758231591 <1758231591@qq.com>
2021-01-20 08:53:35 +0800

    update 微信小程序

--- front_end/微信小程序/test/pages/tests/movable-view/movable-view.js
+++ front_end/微信小程序/test/pages/tests/movable-view/movable-view.js
@@ -5,62 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    x: 0,
+    y: 0,
+    scale: 2,
+    // 当前x的值
+    currentX: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onMovableViewChange(e) {
+    console.log("change", e.detail)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  tap(e) {
+    let kind = parseInt(e.currentTarget.dataset.kind);
+    if (!kind) {
+      this.setData({
+        x: 30,
+        y: 30
+      })
+    } else {
+      this.setData({
+        x: 0,
+        y: 0
+      })
+    }
   }
 })
