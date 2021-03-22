e6a93c641329433e835181a6e246d2ca7320b13a
dusenyao <1758231591@qq.com>
2021-03-22 20:00:03 +0800

    update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 6,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module"
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    indent: ["error", 2]
+  }
 };
