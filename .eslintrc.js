12a0b4ab5cc32b9e1683bb969e8a05d16148bc48
1758231591 <1758231591@qq.com>
2021-03-31 01:44:37 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,17 @@
 module.exports = {
-  parser: "babel-eslint",
+  root: true,
   env: {
     browser: true,
     node: true
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parserOptions: {
     ecmaVersion: 6,
-    sourceType: "module"
+    sourceType: 'module',
+    parser: 'babel-eslint'
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   rules: {
-    indent: ["error", 2]
+    indent: ['error', 2]
   }
 };
