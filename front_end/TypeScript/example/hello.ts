4bbc1a14d563be82b3b9260329d87eb9105ddfc9
dusenyao <1758231591@qq.com>
2021-11-17 01:36:23 +0800

    update TypeScript

--- front_end/TypeScript/example/hello.ts
+++ front_end/TypeScript/example/hello.ts
@@ -1,9 +1,4 @@
-declare const F: {
-  new (x: number): number; // 构造签名
-  (x: number): number; // 调用签名
-};
-
-// 作为普通函数调用
-const a: number = F(1);
-// 作为构造函数调用
-const b: number = new F(1);
+interface A {
+  [prop: string]: 0 | 1;
+  [prop: number]: number; // 编译错误
+}
