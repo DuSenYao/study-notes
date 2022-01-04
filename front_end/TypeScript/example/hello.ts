78fce2f85b651c9a22a37cae800b44a157c0f25a
dusenyao <1758231591@qq.com>
2022-01-04 18:11:19 +0800

    update

--- front_end/TypeScript/example/hello.ts
+++ front_end/TypeScript/example/hello.ts
@@ -1,12 +1,8 @@
-
-class A {
-  a = 1;
-  getA() {
-    return () => {
-      return this.a;
-    };
-  }
+function query(...args: [string, number, boolean]) {
+  const d: string = args[0];
+  const n: number = args[1];
+  const b: boolean = args[2];
+  console.log(d, n, b);
 }
 
-const b = new A().getA();
-console.log(b());
+query('1', 2, false);
